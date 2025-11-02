feb654e4065104d492129ff48533770b5c22fbfd
emerson sabino <emersonsabino34@gmail.com>
2025-11-02 15:32:37 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+
+// js   - ( não retorna o html )
+// jsx  - ( retorna o html )
+
+// ts   - ( não retorna o html  )
+// tsx  - ( retorna o html input, select tipages, user )
+
+
+// typescript é mantido pela microsoft
+// scriptlet ( é neutro sem formatação sem dados )
+// div, h1, h2, ul, input ( )
+// compoennt
+
+export default function Home() {
+  return (
+      <div>
+        <h1 className="text-3xl text-orange-500">Emerson</h1>
+      </div>
+  );
+}
