9835b8fd8b6f8773f5e41c5d130cbace4021fa3f
emerson sabino <emersonsabino34@gmail.com>
2025-11-05 22:55:26 +0000

    updated

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,12 @@
+'use client";'
+
+import { ProductButton } from "./productButton";
+
 export default function ProductsPage() {
   return (
     <div>
       <h1>Products Page</h1>
+      <ProductButton label="Buy Now" icon={<span>🛒</span>} />
       <p>Welcome to the products page. Here you can find a variety of products.</p>
     </div>
   );
--- app/products/productButton.tsx
+++ app/products/productButton.tsx
@@ -2,12 +2,12 @@ interface ButtonProps{
     label: string;
     className?: string;
     icon?: React.ReactNode;
-    onClick: () => void;
+    onClick?: () => void;
 }
 
 export function ProductButton({ label, className, icon, onClick }: ButtonProps) {
   return (
-    <button className={`product-button ${className}`} onClick={onClick}>
+    <button className={`product-button bg-slate-800 text-white p-2 rounded ${className}`} onClick={onClick}>
       {icon && <span className="icon">{icon}</span>}
       {label}
     </button>
