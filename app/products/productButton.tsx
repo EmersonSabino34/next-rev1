94f6e5a8d79ede580388d4a884b3a9020aaaaa42
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-07-22 14:49:05 +0100

    Update productButton.tsx

--- app/products/productButton.tsx
+++ app/products/productButton.tsx
@@ -5,6 +5,7 @@ interface ButtonProps{
     onClick?: () => void;
 }
 
+
 export function ProductButton({ label, className, icon, onClick }: ButtonProps) {
   return (
     <button className={`product-button bg-slate-800 text-white p-2 rounded ${className}`} onClick={onClick}>
